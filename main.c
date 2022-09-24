a25f7e3af25699bdee539668bd3314414b306a1f
Satyamrai8707 <114270778+Satyamrai8707@users.noreply.github.com>
2022-09-24 08:58:53 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+   float radius;
+   
+   printf("Write a radius of Circle =");
+   scanf("%f", &radius);
+   printf("Area of circle = %f", 3.14 * radius * radius);
+     
+    return 0;
+}
